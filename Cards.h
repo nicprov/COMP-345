fbd122d24407c460a2eec93fb301058d728a00c2
Nicolas Provencher <nick@onintime.com>
2021-10-07 11:25:05 -0400

    Add Cards mostly finished

--- Cards.h
+++ Cards.h
@@ -1,28 +1,60 @@
 #ifndef TEST1_CARDS_H
 #define TEST1_CARDS_H
+#include <vector>
 
 class Card
 {
 public:
+    enum Card_Type
+    {
+        bomb,
+        reinforcement,
+        blockage,
+        airlift,
+        diplomacy
+    };
+    Card(Card_Type&);
+    // Copy constructor
+    Card(const Card&);
+    // Assignment operator
+    void operator= (Const Card&);
+    // Stream output operator
+    friend std:ostream& operator<< (std:ostream&, const Card&);
     void play();
-
 private:
     Card_Type type;
+};
 
+class Hand
+{
+public:
+    Hand();
+    // Copy constructor
+    Hand(Const Hand&);
+    // Assignment operator
+    void operator= (Const Card&);
+    // Stream output operator
+    friend std:ostream& operator<< (std:ostream&, const Hand&);
+    void addCard();
+    void removeCard();
+private:
+    std::vector<Card> cards;
 };
 
 class Deck{
 public:
+    Deck();
+    // Copy constructor
+    Deck(Const Deck&);
+    // Assignment operator
+    void operator= (Const Deck&);
+    // Stream output operator
+    friend std:ostream& operator<< (std:ostream&, const Deck&);
     void draw();
+    void addCard();
+    void removeCard();
+private:
+    std::vector<Card> cards;
 
-};
-
-enum Card_Type
-{
-   bomb,
-   reinforcement,
-   blockage,
-   airlift,
-   diplomacy
 };
 #endif //TEST1_CARDS_H
