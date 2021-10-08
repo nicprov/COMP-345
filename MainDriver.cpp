87566a06b68e1072737e685f38563562ca2c2ce5
Nicolas Provencher <nick@onintime.com>
2021-10-07 21:22:35 -0400

    Add testing framework

--- MainDriver.cpp
+++ MainDriver.cpp
@@ -2,6 +2,10 @@
 #include "GameEngineDriver.cpp"
 
 int main(){
+    cout << "Running Cards Driver" << endl;
+    cout << "-----------------------" << endl;
     cardsDriver();
+    cout << endl << endl << "Running Game Engine Driver" << endl;
+    cout << "-----------------------" << endl;
     gameEngineDriver();
 }
