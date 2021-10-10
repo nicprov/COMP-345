e58029b52c769c80455ec0e6cb9765c339b41d4e
Nicolas Provencher <nick@onintime.com>
2021-10-10 18:21:50 -0400

    Changes to CMake file

--- tests/Test_Cards.cpp
+++ tests/Test_Cards.cpp
@@ -1,6 +1,6 @@
 #include <catch2/catch_test_macros.hpp>
 #include <iostream>
-#include "../Cards.h"
+#include "../src/Cards.h"
 
 /**
  * CARD Test cases
--- tests/Test_GameEngine.cpp
+++ tests/Test_GameEngine.cpp
@@ -1,6 +1,6 @@
 #include <catch2/catch_test_macros.hpp>
 #include <iostream>
-#include "../GameEngine.h"
+#include "../src/GameEngine.h"
 
 /**
  * GameEngine Test cases
