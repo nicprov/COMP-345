25327319d86b8b7e9e0955f452187260693f01e0
Nicolas Provencher <nick@onintime.com>
2021-10-12 10:37:43 -0400

    Fix builds with Player

--- src/PlayerDriver.cpp
+++ src/PlayerDriver.cpp
@@ -7,7 +7,7 @@ void PlayerDriver(){
     OrderList* orderlist = new OrderList();
     Hand* hand = new Hand();
     string territory = "";
-    Player* p = new Player( hand, orderlist);
+    Player* p = new Player(*hand, *orderlist);
     Order* deploy = new Deploy(Order::OrderType::deploy);
     p->issueOrder(deploy);
     cout << p;
