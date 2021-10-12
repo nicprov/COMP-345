4b930e7dc6d5e5ccdf041f1fd706dfedc5908171
adDigo <73916391+adDigo@users.noreply.github.com>
2021-10-12 19:53:05 -0400

    added comments to player.cpp

--- src/Player.cpp
+++ src/Player.cpp
@@ -1,6 +1,10 @@
 #include "Player.h"
 
 //Player methods
+
+/**
+ * Destructor
+ */
 Player::~Player()
 {
     delete this->name;          //deallocate memory
@@ -11,12 +15,22 @@ Player::~Player()
     orderList = NULL;
 }
 
+/**
+ * Player default constructor
+ * @param name
+ */
 Player::Player(const std::string &name) {
     this->name = new std::string(name);
     this->hand = new Hand();
     this->orderList = new OrderList();
 }
 
+/**
+ * Player Constructor
+ * @param hand
+ * @param orderlist
+ * @param name
+ */
 Player::Player(const Hand &hand, const OrderList &orderlist, const std::string &name)
 {
     this->hand = new Hand(hand);
@@ -24,6 +38,10 @@ Player::Player(const Hand &hand, const OrderList &orderlist, const std::string &
     this->name = new std::string(name);
 }
 
+/**
+ * Player copy constructor
+ * @param player
+ */
 Player::Player(const Player &player)
 {
     this->hand = new Hand(*player.hand);
@@ -31,6 +49,11 @@ Player::Player(const Player &player)
     this->name = new std::string(*player.name);
 }
 
+/**
+ * Card assignment operator
+ * @param player
+ * @return reference to Player
+ */
 Player& Player::operator= (const Player &player)
 {
     this->hand = new Hand(*player.hand);
@@ -39,19 +62,38 @@ Player& Player::operator= (const Player &player)
     return *this;
 }
 
+/**
+ * Stream insertion operator for Player
+ * @param stream
+ * @param player
+ * @return output stream
+ */
 std::ostream &operator<<(std::ostream &stream, const Player &player)
 {
     return stream << "Player(" << *player.name << "): " << *player.hand << ", " << *player.orderList;
 }
 
+/**
+ * Add order to orderlist
+ * @param order
+ */
 void Player::issueOrder(Order* order) {
     this->orderList->add(order);
 }
 
+/**
+ * Comparison of player by looking at its type
+ * @param player
+ * @return bool value of true (match) or false (does not match)
+ */
 bool Player::operator==(const Player &player) const {
     return *this->name == *player.name && *this->orderList == *player.orderList && *this->hand == *player.hand;
 }
 
+/**
+ * Create and display territories to defend
+ * @return content of territory
+ */
 std::vector<Territory *> Player::toDefend() {
     auto* territoriesToDefend = new std::vector<Territory*>;
     Territory* territory = new Territory(1,"Alabama",2);
@@ -59,6 +101,10 @@ std::vector<Territory *> Player::toDefend() {
     return *territoriesToDefend;
 }
 
+/**
+ * Create and display territories to attack
+ * @return content of territory
+ */
 std::vector<Territory *> Player::toAttack() {
     auto* territoriesToAttack = new std::vector<Territory*>;
     Territory* territory = new Territory(1,"Alabama",2);
@@ -66,12 +112,19 @@ std::vector<Territory *> Player::toAttack() {
     return *territoriesToAttack;
 }
 
-
+/**
+ * Get Player name
+ * @return name
+ */
 std::string &Player::getName()
 {
     return *this->name;
 }
 
+/**
+ * Get Orderlist
+ * @return orderList
+ */
 OrderList &Player::getOrderList()
 {
     return *this->orderList;
